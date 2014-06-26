5b77aeb462d035e10296cdddd9366a55381f1d72
makemek <makemek101@hotmail.com>
2014-06-26 20:45:31 +0700

    define add content page

--- public/add_content.php
+++ public/add_content.php
@@ -1,5 +1,33 @@
-<?php
+<title>Add new content</title>
+<?php include('../includes/layout/header.php'); ?>
+<?php include('../includes/navigation.php'); ?>
 
 
+
+<div id="main">
+	<?php echo Navigation::getInstance()->getHTML(); ?>	
+
+<?php if(!isset($_GET['add'])) { ?>
+	<div id="page">
+		<form action="add_content.php" method="post">
+			Add: 
+			<select name="add">
+				<option value="tenant">Tenant</option>
+				<option value="priv">Privilage</option>
+				<option value="Branch">Branch</option>
+			</select>
+		<input type="submit" name="submit" value="GO"/>
+		</form>
+		<hr />
+	</div>
+
+<?php } else { 
+
+
+}
 
 ?>
+
+</div>
+
+<?php include('../includes/layout/footer.php'); ?>
