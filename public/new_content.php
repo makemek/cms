afcd952b0bd3b4efbc06ec1ed344cf0beca22441
makemek <makemek101@hotmail.com>
2014-06-27 11:15:35 +0700

    Add new_content.php

---
+++ public/new_content.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Data insertion will be performed on this page.
+Input: form from add content $_GET['add'] 
+*/
+
+// what type of form to add ?
+
+
+// get data
+
+
+// issue a query
+
+
+// insert to a database
+
+
+// redirect back to add_content.php
+
+?>
