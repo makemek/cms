d29b2b21061d27618d070915e8b5e419a4c9f23e
Makemek <makemek101@hotmail.com>
2014-06-28 16:16:28 +0700

    commit database class & config

---
+++ includes/database/database.php
@@ -0,0 +1,56 @@
+<?php
+require_once('config.php');
+
+class MySQLDatabase
+{
+    private $db;
+
+    public function open_connection()
+    {
+        try {
+            $this->db = new PDO('mysql:' . DB_HOST . ';dbname=' . DB_NAME . ';',
+            DB_USER, DB_PASSWORD);
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function close_connection()
+    {
+        try {
+        $this->db = null;
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function query($sql) {
+        $result = $this->db->fetch(PDO::FETCH_ASSOC);
+        $this->confirm_query($result);
+        return $result;
+    }
+
+    private function confirm_query($result) {
+        if (!$result) {
+            die("Database query failed: " . mysql_error());
+        }
+    }
+
+    public function mysql_prep($value) {
+        $magic_quotes_active = get_magic_quotes_gpc(); // i.e PHP >= v4.3.0
+        $new_enough_php = function_exists("mysql_real_escape_string");
+        if($new_enough_php) { // PHP v4.3.0 or higher
+            // undo any magic quote effects so mysql_real_escape_string can do the work
+            if($magic_quotes_active) { $value = stripslashes($value); }
+            $value = mysql_real_escape_string($value);
+        } else { // before PHP v4.3.0
+            // if magic quotes aren't already on then add slashes manually
+            if(!$magic_quotes_active) { $value = addslashes($value); }
+            // if magic quotes are active, then the slashes already exist
+        }
+        return $value;
+    }
+}
+
+$db = new MySQLDatabase();
