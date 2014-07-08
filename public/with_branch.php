ddd87f5aae485a62025193d0bea57d941c81032d
Makemek <makemek101@hotmail.com>
2014-07-08 17:02:01 +0700

    Create page with_branch.php

---
+++ public/with_branch.php
@@ -0,0 +1,12 @@
+<?php
+require_once('../includes/database/database.php');
+require_once('../includes/database/table_config.php');
+include_once('../includes/layout/header.php');
+
+$db = new MySQLDatabase(DB_TRUEYOU);
+?>
+
+
+<?php include_once('../includes/layout/footer.php'); ?>
+
+
