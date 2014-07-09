d88eeb34b5dad76845e189d82a5b562243e29767
Makemek <makemek101@hotmail.com>
2014-07-09 21:55:24 +0700

    Display branch table on load with_branch.php

--- public/with_branch.php
+++ public/with_branch.php
@@ -44,8 +44,10 @@ if(isset($_GET['store'])) {
     <table style="width:300px">
 
         <?php
-        echo table_header($db, $table);
-        $query = "SELECT * FROM ? WHERE ";
+        echo table_header($db, trueyou\Branch_tbl::name());
+        $query = "select * from branch where bname = (select bname from tenant_branch)";
+        $result = $db->query($query);
+
         ?>
 
     </table>
@@ -54,7 +56,7 @@ if(isset($_GET['store'])) {
 else {
     ?>
 
-    <form action="">
+    <form action="with_branch.php">
         <table style="width:300px">
             <?php
             echo table_header($db, trueyou\Branch_tbl::name());
@@ -67,11 +69,11 @@ else {
             echo 'Total Result: '.$stmt->rowCount().'<br/>';
             while($row = $stmt->fetch(PDO::FETCH_NUM)) {
                 echo '<tr>';
-                echo "<td><input type=\"checkbox\" value=\"$row[0]\"></td>";
+                echo "<td><input type=\"checkbox\" name=\"$row[0]\"></td>";
                 foreach($row as $col)
                     echo '<td>'.$col.'</td>';
-                echo "<td><input type=\"text\" name=\"floor1[]\"</td>";
-                echo "<td><input type=\"text\" name=\"floor2[]\"</td>";
+                echo "<td><input type=\"number\" name=\"floor1[]\" min=\"0\" max=\"9999\"></td>";
+                echo "<td><input type=\"number\" name=\"floor2[]\" min=\"0\" max=\"9999\"></td>";
                 echo '</tr>';
             }
             ?>
@@ -81,5 +83,75 @@ else {
 <?php }
 
 include_once('../includes/layout/footer.php');
+
+class Table_form extends Form implements Record {
+
+    private $header;
+    private $row;
+
+    private $db_table_name;
+
+    const STRING_FIELD = 0;
+    const NUMERIC_FIELD = 1;
+
+    private $string_field = array();
+    private $numeric_field = array();
+
+    public function __construct($header, $row=null, $db_table_name) {
+        $this->db_table_name = $db_table_name;
+
+        $this->header = $header;
+        $this->row = $row;
+    }
+
+    public function get_associate_db_table()
+    {
+        return $this->db_table_name;
+    }
+
+    public function fetch()
+    {
+        // TODO: Implement fetch() method.
+    }
+
+    public function form()
+    {
+        // TODO: Implement form() method.
+
+    }
+    protected function get_all_string_fields()
+    {
+        // TODO: Implement get_all_string_fields() method.
+    }
+
+    protected function get_all_numeric_fields()
+    {
+        // TODO: Implement get_all_numeric_fields() method.
+    }
+
+    protected function validate($input)
+    {
+        // TODO: Implement validate() method.
+    }
+
+    public function add_row($row, $has_input, $field_type) {
+        if($has_input) {
+            switch($field_type) {
+                case self::STRING_FIELD:
+                    $this->string_field[] = $field_type;
+                    break;
+                case self::NUMERIC_FIELD:
+                    $this->numeric_field[] = $field_type;
+                    break;
+
+                default:
+                    echo 'Incorrect field_type assume string type';
+                    $this->string_field[] = $field_type;
+            }
+        }
+
+        $this->row[] = $row;
+    }
+}
 
 
