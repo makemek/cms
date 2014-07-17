e558fff558fca758527795d439becafef8106496
Makemek <makemek101@hotmail.com>
2014-07-17 15:50:46 +0700

    Can specify ignore line and select column

--- public/import.php
+++ public/import.php
@@ -4,15 +4,42 @@ require_once('../includes/database/database.php');
 include_once('../includes/layout/header.php');
 
 define('CSV', 'application/vnd.ms-excel');
+define('SKIP_LINE', 'skip_line');
+define('COLUMN', 'col');
+
+$db = new MySQLDatabase(DB_TRUEYOU);
 ?>
 
 <div id="page">
     <h2>Import From CSV File</h2>
+
+    <?php $tbl = array(
+        'Branch' => trueyou\Branch_tbl::name(),
+        'Privilege' => trueyou\Priv_tbl::name(),
+        "Privilege's branch" => trueyou\Priv_branch_tbl::name(),
+        "Tenant's branch" => trueyou\Tenant_branch_tbl::name()
+    ); ?>
+
     <form action="import.php" enctype="multipart/form-data" method="POST">
-        Branch: <input name="<?php echo trueyou\Branch_tbl::name(); ?>" type="file" accept=".csv" /><br />
-        Privilege: <input name="<?php echo trueyou\Priv_tbl::name(); ?>" type="file" accept=".csv" /><br />
-        Privilege's Branch: <input name="<?php echo \trueyou\Priv_branch_tbl::name(); ?>" type="file" accept=".csv" /><br />
-        Tenant's Branch: <input name="<?php echo \trueyou\Tenant_branch_tbl::name(); ?>" type="file" accept=".csv" /><br />
+
+        <?php
+        foreach($tbl as $name => $table) { ?>
+            <fieldset>
+                <legend><?php echo $name; ?></legend>
+
+                <input type="file" name="<?php echo $table ?>" accept=".csv" />
+                Ignore First: <input type="number" min="0" name="<?php echo $table;?>[line]" value="0" /> Lines
+                <br />
+
+                <?php
+                $col = $db->get_table_column_name($table);
+                foreach($col as $c)
+                    echo "<input type=\"checkbox\" name=\"{$table}[col][]\" value=\"{$c}\"/>" . $c;
+            ?>
+            </fieldset>
+            <br />
+        <?php } ?>
+
         <input type="submit" name="submit" value="Upload" />
         <input type="reset" name="reset" value="Reset" />
     </form>
@@ -22,8 +49,6 @@ define('CSV', 'application/vnd.ms-excel');
     <?php
     if(!isset($_POST['submit']))
         die();
-
-    $db = new MySQLDatabase(DB_TRUEYOU);
 
     foreach($_FILES as $table => $file) {
         if($file['error'] != UPLOAD_ERR_OK || $file['type'] !== CSV) {
@@ -40,7 +65,12 @@ define('CSV', 'application/vnd.ms-excel');
         $query .= "FIELDS TERMINATED BY ',' ";
         $query .= "ENCLOSED BY '\"' ";
         $query .= "LINES TERMINATED BY '\n' ";
-
+        $query .= "IGNORE {$_POST[$table]['line']} LINES ";
+        $query .= "(";
+        foreach($_POST[$table]['col'] as $field)
+            $query .= $field . ',';
+        $query = rtrim($query, ',');
+        $query .= ") ";
 
         $stmt = $db->prepare($query);
 
@@ -50,13 +80,15 @@ define('CSV', 'application/vnd.ms-excel');
             echo "Table: " . $table . '<br />';
             echo "Rows affacted: " . $stmt->rowCount() . '<br />';
 
-            unlink(UPLOAD_DIR . $file['name']);
-
         } catch (PDOException $e) {
             echo "<strong><span style='color:red'>CSV Import Failure</span></strong><br/>";
             die($e->getMessage());
         }
+
+        unlink(UPLOAD_DIR . $file['name']);
     }
+
+
 
     ?>
 
